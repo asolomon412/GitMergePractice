f6ab5b6f7a0923eb67a4a3b3a0188eba5ad23c49
benjamin <benjamin.mcbrayer@gmail.com>
2018-10-25 13:46:57 -0400

    added another line

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,6 +4,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Goodbye, cruel world!");
+		System.out.println("Seriously, we're done.");
 	}
 
 }
