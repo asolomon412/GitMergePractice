4b900616165ec2e7e45e4db949ccffa51030b990
Antonella Solomon <antonellasolomon@gmail.com>
2018-10-25 13:44:12 -0400

    added a line

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,6 +2,8 @@
 public class MainApp {
 
 	public static void main(String[] args) {
+		
+		System.out.println("My name is Antonella!");
 		System.out.println("Hello World!");
 		System.out.println("Goodbye, cruel world!");
 	}
